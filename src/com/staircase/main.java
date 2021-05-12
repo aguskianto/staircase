c92d9c836c51f14dc949e8f642678586cd679dc3
agus kianto <agus.kianto@gmail.com>
2021-05-12 07:27:30 +0700

    enhance application flow

--- src/com/staircase/main.java
+++ src/com/staircase/main.java
@@ -2,12 +2,14 @@ package com.staircase;
 
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class main {
 	public static void main(String[] args) {
-Scanner scanner = new Scanner(System.in);
+		Scanner scanner = new Scanner(System.in);
 		String input = "";
 		
 	    do{
@@ -20,32 +22,53 @@ Scanner scanner = new Scanner(System.in);
 			System.out.println("To quit the program type exit or press Ctrl + C");
 			System.out.println("");
 			
+			// 1. get input
 	        input = scanner.nextLine();
 	        
-	        try {
-	        	int max_height = Integer.parseInt(input);
+	        // 2. check if exit command
+	        if (input.equals("exit")) {
+	        	break;
+	        }
+	        
+	        // 3. check if input is not a number
+	        Pattern p = Pattern.compile("[A-Z,a-z,&%$#@!()*^]");
+	        Matcher m = p.matcher(input);
+	        
+	        if (m.find()) {
+	        	System.out.println("Error: ");
+				System.out.println("Argument is not a number");
+				System.out.println("");
+	        } else {
+	        	// 4. execute if input is number between 1 - 100
+	        	Long max_height = convertStringToLong(input);
 		        
 		        if (max_height > 0 && max_height <= 100) {
-					IntStream serials = IntStream.range(1, max_height + 1);
-					
+		        	int max_height_value = max_height.intValue();
+		        	
+		        	IntStream serials = IntStream.range(1, max_height_value + 1);
+						
 					System.out.println("Output: ");
-					
+						
 					serials
-					.mapToObj(i -> new String(new char[max_height - i]).replace('\0', ' ') + new String(new char[i]).replace('\0', '#'))
+					.mapToObj(i -> new String(new char[max_height_value - i]).replace('\0', ' ') + new String(new char[i]).replace('\0', '#'))
 					.forEach(System.out::println);
-					
+						
 					System.out.println("");
-				} else {
-					System.out.println("Error: ");
+		        } else {
+		        	System.out.println("Error: ");
 					System.out.println("Argument must be between 1 to 100");
 					System.out.println("");
-				}
-	        } catch (Error err) {
-	        	System.out.println("Error: ");
-	        	System.out.println("Input is not a number");
-	        	System.out.println("");
+		        }
 	        }
 	    } while(!input.equals("exit"));
 	}
 
+	private static Long convertStringToLong(String input) {
+	    try {
+	    	Long result = Long.parseLong(input);
+	        return result;
+	    } catch(Error e) {
+	        return null;
+	    }
+	}
 }
